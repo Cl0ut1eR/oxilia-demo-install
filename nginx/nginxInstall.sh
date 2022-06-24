c3685149279bb9b9eb653931da255a8c994e8fbb
Cl0ut1eR <etiennecloutier00@gmail.com>
2022-06-24 11:07:23 +0200

    add test

--- nginx/nginxInstall.sh
+++ nginx/nginxInstall.sh
@@ -1,6 +1,6 @@
 docker network create nginx
 docker-compose up -d
 
-#   docker run --rm --name nginx-dummy -e VIRTUAL_HOST=test.oxiliatest.ddns.net -e LETSENCRYPT_HOST=test.oxiliatest.ddns.net -e VIRTUAL_PORT=80 --network nginx -d nginx:latest
+#   docker run --rm --name nginx-dummy -e VIRTUAL_HOST=oxiliatest.ddns.net -e LETSENCRYPT_HOST=oxiliatest.ddns.net -e VIRTUAL_PORT=80 --network nginx -d nginx:latest
 #   docker run --name nextcloud --network nginx -e VIRTUAL_HOST="oxiliatest.ddns.net" -e LETSENCRYPT_HOST="oxiliatest.ddns.net" -d nextcloud:19.0.2
-#   docker run --rm --name test -it -d -e VIRTUAL_HOST="oxiliatest.ddns.net" -e LETSENCRYPT_HOST="oxiliatest.ddns.net" -e VIRTUAL_PORT=81 --network nginx yeasy/simple-web:latest
+#   docker run --rm --name test -it -d -e VIRTUAL_HOST="oxiliatest.ddns.net" -e LETSENCRYPT_HOST="oxiliatest.ddns.net" -e VIRTUAL_PORT=80 --network nginx yeasy/simple-web:latest
