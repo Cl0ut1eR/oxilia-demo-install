60051feff454f1edf88997f14be3adebf3eff6ab
Cl0ut1eR <etiennecloutier00@gmail.com>
2022-06-27 10:48:52 +0200

    better interface

--- clear.sh
+++ clear.sh
@@ -3,5 +3,5 @@ bash ResetDocker.sh
 bash DockerRemove.sh
 cd ..
 sudo rm -rf self-runner/actions-runner
-printf "\n\nSupprimer les anciens runners : \n /etc/systemd/system/"
+printf "\n\n\n\n\n\nSupprimer les anciens runners : \n /etc/systemd/system/\n\n"
 ls /etc/systemd/system/
