26d0cecfcc43c9280a004649adf6edd96c095dea
Cl0ut1eR <etiennecloutier00@gmail.com>
2022-06-27 10:47:48 +0200

    add runner removal

--- clear.sh
+++ clear.sh
@@ -3,3 +3,5 @@ bash ResetDocker.sh
 bash DockerRemove.sh
 cd ..
 sudo rm -rf self-runner/actions-runner
+printf "\n\nSupprimer les anciens runners : \n /etc/systemd/system/"
+ls /etc/systemd/system/
