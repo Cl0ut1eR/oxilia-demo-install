49b1fd3d1826e9d420374e55479302cfc6e801d2
Cl0ut1eR <etiennecloutier00@gmail.com>
2022-06-28 12:15:46 +0200

    add info for errors

--- partie2.sh
+++ partie2.sh
@@ -4,4 +4,6 @@ bash setupPart2.sh
 clear -x
 printf "*******************************************************************************
 *       Terminé, vérifier sur Github que votre runner est en mode idle.       *
+*                      Si ce n'est pas le cas, visitez :                      *
+*           https://github.com/Cl0ut1eR/oxilia-demo-install#erreurs           *
 *******************************************************************************\n"
