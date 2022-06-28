77345ead26c04e73c0f3704d8afe356519670755
Cl0ut1eR <etiennecloutier00@gmail.com>
2022-06-28 17:59:59 +0200

    add user to action

--- self-runner/setupPart2.sh
+++ self-runner/setupPart2.sh
@@ -1,6 +1,9 @@
+sudo useradd selfrunner
+sudo chown -R selfrunner ./actions-runner
+sudo usermod -a -G docker selfrunner
 cd ./actions-runner
 # Start self runner as a service
-sudo ./svc.sh install
+sudo ./svc.sh install selfrunner
 sudo ./svc.sh start
 
 cd ..
