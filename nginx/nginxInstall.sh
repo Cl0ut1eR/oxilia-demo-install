e30082871803abf2e24f976c5a5b57ea420ab1b4
Cl0ut1eR <etiennecloutier00@gmail.com>
2022-06-28 14:03:30 +0200

    sudo docker

--- nginx/nginxInstall.sh
+++ nginx/nginxInstall.sh
@@ -1,7 +1,7 @@
 # Create docker network for nginx
-docker network create nginx
+sudo docker network create nginx
 # Create containers
-docker-compose --env-file .env up -d
+sudo docker-compose --env-file .env up -d
 # add more nginx config
-docker cp ./more_conf.conf reverse-proxy:/etc/nginx/conf.d/more_conf.conf
-docker restart reverse-proxy 
+sudo docker cp ./more_conf.conf reverse-proxy:/etc/nginx/conf.d/more_conf.conf
+sudo docker restart reverse-proxy 
