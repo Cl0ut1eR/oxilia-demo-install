8d98ad18a2c9b81ad09c38cffdd855ca7b3cb9ce
Cl0ut1eR <etiennecloutier00@gmail.com>
2022-06-28 09:02:42 +0200

    change clear

--- debug/clear.sh
+++ debug/clear.sh
@@ -1,7 +1,7 @@
-cd ./docker
 bash ResetDocker.sh
 bash DockerRemove.sh
-cd ..
-sudo rm -rf self-runner/actions-runner
-printf "\n\n\n\n\n\nSupprimer les anciens runners : \n /etc/systemd/system/\n\n"
-ls /etc/systemd/system/
+sudo ./self-runner/actions-runner/svc.sh uninstall
+#sudo rm -rf self-runner/actions-runner
+#printf "\n\n\n\n\n\nSupprimer les anciens runners : \n /etc/systemd/system/\n\n"
+#ls /etc/systemd/system/
+echo ligne de suppression du runner
