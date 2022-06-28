4fc0d5dda330b633626c92b51b7b5fb697a787da
Cl0ut1eR <etiennecloutier00@gmail.com>
2022-06-28 11:58:39 +0200

    add confirmation before clear

--- debug/clear.sh
+++ debug/clear.sh
@@ -1,7 +1,14 @@
-bash ResetDocker.sh
-bash DockerRemove.sh
-cd ../self-runner/actions-runner
-sudo ./svc.sh uninstall
 clear -x
-echo "cd ../self-runner/actions-runner"
-echo coller la ligne de suppression du runner
+read -p "Vous allez supprimmer docker ainsi que toutes les informations présentes\n confirmer? oui/non" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[YyOo]$ ]]
+then
+    bash ResetDocker.sh
+    bash DockerRemove.sh
+    cd ../self-runner/actions-runner
+    sudo ./svc.sh uninstall
+    clear -x
+    echo "cd ../self-runner/actions-runner"
+    echo coller la ligne de suppression du runner
+fi
+
