c71e7ddd8b8788dc4466b4d95cc6cdd309f39fae
Cl0ut1eR <etiennecloutier00@gmail.com>
2022-06-29 15:43:33 +0200

    a

--- debug/removeRunner.sh
+++ debug/removeRunner.sh
@@ -2,9 +2,10 @@ printf "Tapez la commande de suppression : \n"
 read COMMAND
 
 cd /home/selfrunner/actions-runner
-sudo -u selfrunner $COMMAND
 
 sudo ./svc.sh uninstall
+sudo -u selfrunner $COMMAND
+
 sudo userdel selfrunner
 sudo rm -fr /home/selfrunner
 echo "old runner services : /etc/systemd/system/"
