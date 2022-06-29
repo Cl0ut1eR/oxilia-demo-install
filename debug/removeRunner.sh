8c56158b5b0417c4689518847993bbe69b4217f4
Cl0ut1eR <etiennecloutier00@gmail.com>
2022-06-29 10:33:14 +0200

    a

--- debug/removeRunner.sh
+++ debug/removeRunner.sh
@@ -1,5 +1,6 @@
-    cd ../self-runner/actions-runner
-    sudo ./svc.sh uninstall
-    clear -x
-    echo "cd ../self-runner/actions-runner"
-    echo coller la ligne de suppression du runner
+cd ../self-runner/actions-runner
+sudo ./svc.sh uninstall
+clear -x
+echo "cd ../self-runner/actions-runner"
+echo "/etc/systemd/system/actions.runner"
+echo coller la ligne de suppression du runner
