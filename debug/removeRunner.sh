605fc59e5ceba5b73c7fcf71d8e0903150812799
Cl0ut1eR <etiennecloutier00@gmail.com>
2022-06-29 10:37:22 +0200

    a

--- debug/removeRunner.sh
+++ debug/removeRunner.sh
@@ -2,5 +2,5 @@ cd ../self-runner/actions-runner
 sudo ./svc.sh uninstall
 clear -x
 echo "cd ../self-runner/actions-runner"
-echo "/etc/systemd/system/actions.runner"
 echo coller la ligne de suppression du runner
+echo "/etc/systemd/system/actions.runner"
--- self-runner/setupPart2.sh
+++ self-runner/setupPart2.sh
@@ -1,6 +1,6 @@
 sudo useradd selfrunner
 sudo chown -R selfrunner:selfrunner ./actions-runner
-#sudo chmod -R 755 ./actions-runner
+sudo chmod -R 755 ./actions-runner
 sudo usermod -a -G docker selfrunner
 cd ./actions-runner
 # Start self runner as a service
