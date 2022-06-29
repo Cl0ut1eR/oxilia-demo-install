c057f9e94ad34268fb4afe54ecda320458b4a8af
Cl0ut1eR <etiennecloutier00@gmail.com>
2022-06-29 10:39:54 +0200

    test

--- self-runner/setupPart2.sh
+++ self-runner/setupPart2.sh
@@ -1,7 +1,8 @@
 sudo useradd selfrunner
-sudo chown -R selfrunner:selfrunner ./actions-runner
-sudo chmod -R 755 ./actions-runner
+#sudo chown -R selfrunner:selfrunner ./actions-runner
+#sudo chmod -R 755 ./actions-runner
 sudo usermod -a -G docker selfrunner
+sudo usermod -a -G $USER selfrunner
 cd ./actions-runner
 # Start self runner as a service
 sudo ./svc.sh install selfrunner
